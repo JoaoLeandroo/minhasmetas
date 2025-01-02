f5a1bf109d1787602278fa7c4db04b285f568bcd
JoaoLeandroo <new.joaoleandro@gmail.com>
2025-01-02 10:43:10 -0300

    pagina login

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -1,17 +1,32 @@
+"use client";
+import { LoginUser } from "@/lib/action";
 import { Button } from "../ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
 import Link from "next/link";
+import { useActionState, useState } from "react";
 
 const LoginForm = () => {
+  const [state, formAction, isPending] = useActionState(LoginUser, null);
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+  
   return (
     <Card className="max-w-xl w-full">
       <CardHeader>
         <CardTitle>Meta treinos - Login</CardTitle>
       </CardHeader>
       <CardContent>
-        <form className="flex flex-col gap-y-3">
+        <form className="flex flex-col gap-y-3" action={formAction}>
           <div>
             <Label htmlFor="email">Email</Label>
             <Input
@@ -20,7 +35,16 @@ const LoginForm = () => {
               id="email"
               placeholder="Digite seu email"
               className="h-11"
+              value={formData.email}
+              onChange={handleChange}
             />
+            <div id="name-error" aria-live="polite" aria-atomic="true">
+              <p className="mt-2 text-sm text-red-500">
+                {state?.Error && state?.Error?.email
+                  ? state?.Error?.email?.[0]
+                  : null}
+              </p>
+            </div>
           </div>
 
           <div>
@@ -31,17 +55,32 @@ const LoginForm = () => {
               id="password"
               placeholder="Sua senha"
               className="h-11"
+              value={formData.password}
+              onChange={handleChange}
             />
+            <div id="name-error" aria-live="polite" aria-atomic="true">
+              <p className="mt-2 text-sm text-red-500">
+                {state?.Error && state?.Error?.password
+                  ? state?.Error?.password?.[0]
+                  : null}
+              </p>
+            </div>
           </div>
 
-          <Button type="submit" className="w-full mt-4 h-11">
+          <Button disabled={isPending} type="submit" className="w-full mt-4 h-11">
             Entrar
           </Button>
         </form>
         <div className="mt-4 w-full text-center">
-            <p>
-                Não possui conta? <Link className="text-blue-500 hover:opacity-75 transition duration-200" href={"/register"}>Cadastre-se</Link>
-            </p>
+          <p>
+            Não possui conta?{" "}
+            <Link
+              className="text-blue-500 hover:opacity-75 transition duration-200"
+              href={"/register"}
+            >
+              Cadastre-se
+            </Link>
+          </p>
         </div>
       </CardContent>
     </Card>
--- src/lib/action.ts
+++ src/lib/action.ts
@@ -1,5 +1,7 @@
 "use server";
 import db from "./db";
+import { z } from "zod"
+import { redirect } from "next/navigation"
 
 export default async function registerUser(_prev: any, formDate: FormData) {
   const entries = Array.from(formDate.entries());
@@ -45,4 +47,54 @@ export default async function registerUser(_prev: any, formDate: FormData) {
     message: "Usuário cadastrado com sucesso!",
     success: true,
   };
+}
+
+
+const LoginSchema = z.object({
+    email: z.string().email({message: "Informe um email valido."}).transform(email => email.toLowerCase()),
+    password: z.string().min(6, {message: "A senha deve ter no minimo 6 caracters."}),
+})
+
+type LoginUserError = {
+    email?: string[];
+    password?: string[];
+};
+
+export const LoginUser = async (_prevState: any, formData: FormData): Promise<{ Error?: LoginUserError }> => {
+    const validateFields = LoginSchema.safeParse(
+        Object.fromEntries(formData.entries())
+      );
+
+      if (!validateFields.success) {
+        return {
+          Error: validateFields.error.flatten().fieldErrors,
+        };
+      }
+
+      const { email, password } = validateFields.data;
+
+      const verifyUser = await db.usuario.findUnique({
+        where: {email}
+      })
+
+      if(!verifyUser) {
+        return {
+            Error: {
+                email: ["Email ou senha incorretos."],
+                password: ["Email ou senha incorretos."]
+            }
+        }
+      }
+
+      if(verifyUser.password != password) {
+        return {
+            Error: {
+                email: ["Email ou senha incorretos."],
+                password: ["email ou senha incorretos."]
+            }
+        }
+      }
+
+      redirect("/dashboard")
+
 }
